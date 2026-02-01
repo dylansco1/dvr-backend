d3958f998a13f09e977ad068bf6a0581db82f43b
unknown <dylanistjppbidlcpfpglv@gmail.com>
2026-02-01 01:39:53 +0000

    initial commit

---
+++ record/record.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+int main(int argc, char* argv[]) {
+    std::string stream, out, time;
+
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+
+        if (arg == "-stream") {
+            if (i + 1 < argc) stream = argv[++i];
+            else {
+                std::cerr << "Missing value for -stream\n";
+                return 1;
+            }
+        }
+        else if (arg == "-out") {
+            if (i + 1 < argc) out = argv[++i];
+            else {
+                std::cerr << "Missing value for -out\n";
+                return 1;
+            }
+        }
+        if (arg == "-time") {
+            if (i + 1 < argc) time = argv[++i];
+            else {
+                std::cerr << "Missing value for -time\n";
+                return 1;
+            }
+        }
+        else {
+            std::cerr << "Unknown argument: " << arg << "\n";
+        }
+    }
+
+    if (stream.empty() || out.empty()) {
+        std::cerr << "Usage: record -stream <URL> -out <OUTFILE> -time <SECONDS>\n";
+        return 1;
+    }
+
+    std::cout << "stream = " << stream << "\n";
+    std::cout << "out = " << out << "\n";
+    std::cout << "time = " << time << "\n";
+
+    int rec = system(("ffmpeg -i " + stream + " -c copy " + "-t " + time + " " + out).c_str());
+}
+
